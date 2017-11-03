1382b0061dbaf82646d5e74264ade0a4d8d66b34
nesmabelkhodja <nb2253@nyu.edu>
2017-11-02 20:29:51 -0400

    Parts 2, 3 -- some progress on part 4

--- src/movies/app.js
+++ src/movies/app.js
@@ -1,1 +1,41 @@
 // app.js
+const express = require('express');
+require('./db');
+const bodyParser = require('body-parser');
+const app = express();
+
+const mongoose = require('mongoose');
+const Movie = mongoose.model('Movie');
+
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/movies', function(req, res) {
+	let constraint = {};  
+	if (typeof (req.query.director) !== 'undefined'  && (req.query.director).length>0){
+	  	constraint = req.query.director; 
+	  }
+
+  Movie.find(constraint, function(err, movies, count) {
+    if(err) {
+      res.send(err); 
+    }
+    res.render( 'index', {
+      movies: movies
+    });
+  });
+});
+
+app.post('/movies', function(req, res) {
+		new Movie({
+		title: req.body.title,
+		director: req.body.director,
+		year: req.body.date
+	}).save(function(err, movies, count){
+
+	res.redirect('/movies');
+	});
+});
+
+app.listen(3000);
--- src/movies/db.js
+++ src/movies/db.js
@@ -1,1 +1,13 @@
 // db.js
+const mongoose = require('mongoose') 
+
+// my schema goes here!
+const Movie = new mongoose.Schema({
+  title: String,
+  director: String,
+  year: Number
+});
+
+mongoose.model('Movie', Movie);
+
+mongoose.connect('mongodb://localhost/hw05');
